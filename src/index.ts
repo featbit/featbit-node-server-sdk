9cff62d21d88490a77428bcec2b3b9097d9f96d7
cosmos-explorer <lian.yang.work@gmail.com>
2023-12-31 19:19:06 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -14,11 +14,9 @@ export * from './Configuration';
 export * from './constants';
 export * from './Context';
 export * from './errors';
-export * from './FbClientCore';
 export * from './FbClientBuilder';
 export * from './IContextProperty';
 export * from './IDataKind';
-export * from './IFbClientCore';
 export * from './IFbClient';
 export * from './IVersionedData';
 
